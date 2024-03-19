28ff60a9f0a57e1782a01512cdfccf6bd04f8c7a
Angel Vazquez <angelvazquez470@gmail.com>
2024-03-18 19:32:12 -0600

    login mobile styles finished. Projects index started

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0 bg-zinc-800">
         <figure class="absolute w-full">
-            <img class="h-full w-full" src="@/../../public/assets/images/fondo_login.png" alt="fondo">
+            <img class="h-screen lg:h-full w-full" src="@/../../public/assets/images/fondo_login.png" alt="fondo">
         </figure>
 
-        <div class="h-[400px] lg:h-[800px] w-[80%] mt-6 px-6 py-4 bg-white overflow-hidden rounded-[30px] z-40 lg:grid grid-cols-2">
+        <div class="h-[800px] w-[80%] mt-6 px-6 py-4 bg-white overflow-hidden rounded-[30px] z-40 lg:grid grid-cols-2">
             <div class="items-center justify-center flex flex-col">
                 <div class="mb-10">
                     <slot name="logo" />
@@ -13,7 +13,7 @@
                     <slot />
                 </div>
             </div>
-            <figure class="w-full">
+            <figure class="w-full mt-4 lg:mt-0">
                 <img class="h-full w-full" src="@/../../public/assets/images/fondo_login_interno.png" alt="fondo">
             </figure>
         </div>
