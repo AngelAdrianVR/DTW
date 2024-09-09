73c3e7219aae7c95e5ecc53b1cbf0df8aad7aedc
Angel Vazquez <angelvazquez470@gmail.com>
2024-09-09 11:54:14 -0600

    apartado de proyectos

--- resources/js/Pages/English/LandingEn.vue
+++ resources/js/Pages/English/LandingEn.vue
@@ -377,58 +377,11 @@
                             Your browser does not support the video element.
                         </video>
                     </div>
-                    <!-- <figure class="rounded-[20px] hidden lg:block">
-                        <img class="rounded-[20px]" src="@/../../public/assets/images/contact.png" />
-                    </figure> -->
                 </div>
             </section>
 
-            <footer class="p-4 text-white bg-[#1A1A1A] h-72 md:h-52 mt-24 md:relative overflow-auto">
-                <figure>
-                    <img class="md:w-[6%] w-[29%]" src="@/../../public/assets/images/white_logo.png" alt="" />
-                </figure>
-                <div class="md:grid grid-cols-3 h-28">
-                    <div class="border-r border-white flex justify-center items-cente">
-                        <p class="text-lg">"Unlocking your online potential"</p>
-                    </div>
-
-                    <div class="border-r border-white flex justify-center items-center">
-                        <div class="flex flex-col">
-                            <h2 class="text-center mb-5 text-xl">Contact</h2>
-                            <div>
-                                <p><i class="fa-solid fa-phone mr-2 mb-3"></i>3322268824</p>
-                                <p>
-                                    <i class="fa-solid fa-envelope mr-2"></i>contacto@dtw.com.mx
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="flex justify-center items-center">
-                        <div class="flex flex-col justify-center items-center">
-                            <h2 class="text-center mb-5 text-xl">Follow us!</h2>
-                            <div>
-                                <a href="https://www.facebook.com/profile.php?id=100094614796876" target="_blank"><i
-                                        class="fa-brands fa-facebook text-2xl border border-white rounded-full px-3 py-2 cursor-pointer mr-5"></i></a>
-                                <a href="https://instagram.com/digital_tw?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D"
-                                    target="_blank"><i
-                                        class="fa-brands fa-instagram text-2xl border border-white rounded-full px-3 py-2 cursor-pointer"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <a class="hidden md:block" href="">
-                    <p class="text-xs mt-3 md:absolute bottom-2 left-5 hover:underline">
-                        Privacy Policy - Cookie Policy
-                    </p>
-                </a>
-                <a class="hidden md:block" href="">
-                    <p class="text-xs mt-3 md:absolute bottom-2 left-96 hover:underline">
-                        Copyright | 2023 | All rights reserved by Digital TW S.A
-                         de C.V.
-                    </p>
-                </a>
-            </footer>
+            <!-- Footer -->
+            <EnglishFooter />
         </main>
     </div>
 </template>
@@ -437,6 +390,7 @@
 import { useForm, Link, Head } from "@inertiajs/vue3";
 import { useToast } from "vue-toastification";
 import EnglishNav from "@/Components/MyComponents/Landing/EnglishNav.vue";
+import EnglishFooter from "@/Components/MyComponents/Landing/EnglishFooter.vue";
 import e3dtest from '@/../../public/assets/images/e3dtest.png';
 import churrotest from '@/../../public/assets/images/churrotest.png';
 import profibratest from '@/../../public/assets/images/profibratest.png';
@@ -523,9 +477,10 @@ export default {
         },
     },
     components:{
-      Link,
-      Head,
-      EnglishNav
+        EnglishFooter,
+        EnglishNav,
+        Link,
+        Head,
     }
 };
 </script>
