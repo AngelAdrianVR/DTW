8de45a1539e4d0b6af93693e29ed2ca5f0e47126
Angel Vazquez <angelvazquez470@gmail.com>
2025-03-03 18:28:22 -0600

    modulo de componentes en panel de administración

---
+++ resources/js/Pages/Component/Show.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        show de componentes
+    </main>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+
+    }
+},
+components:{
+
+},
+props:{
+
+},
+methods:{
+    
+}
+}
+</script>
