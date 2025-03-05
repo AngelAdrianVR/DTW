8562ed6b5aeec16f75cedf822a958d3ace0c6053
Angel Vazquez <angelvazquez470@gmail.com>
2025-03-04 19:15:02 -0600

    casi terminado el módulo completo

---
+++ resources/js/Components/MyComponents/CopyButton.vue
@@ -0,0 +1,184 @@
+<template>
+    <button class="copy">
+        <span
+            class="tooltip z-50"
+            data-text-initial="Copiar"
+            data-text-end="Copiado!"
+        ></span>
+        <span>
+            <svg
+            class="clipboard"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="20"
+            height="20"
+            x="0"
+            y="0"
+            viewBox="0 0 6.35 6.35"
+            style="enable-background:new 0 0 512 512"
+            xml:space="preserve"
+            >
+            <g>
+                <path
+                d="M2.43.265c-.3 0-.548.236-.573.53h-.328a.74.74 0 0 0-.735.734v3.822a.74.74 0 0 0 .735.734H4.82a.74.74 0 0 0 .735-.734V1.529a.74.74 0 0 0-.735-.735h-.328a.58.58 0 0 0-.573-.53zm0 .529h1.49c.032 0 .049.017.049.049v.431c0 .032-.017.049-.049.049H2.43c-.032 0-.05-.017-.05-.049V.843c0-.032.018-.05.05-.05zm-.901.53h.328c.026.292.274.528.573.528h1.49a.58.58 0 0 0 .573-.529h.328a.2.2 0 0 1 .206.206v3.822a.2.2 0 0 1-.206.205H1.53a.2.2 0 0 1-.206-.205V1.529a.2.2 0 0 1 .206-.206z"
+                fill="currentColor"
+                ></path>
+            </g>
+            </svg>
+            <svg
+            class="checkmark"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="18"
+            height="18"
+            x="0"
+            y="0"
+            viewBox="0 0 24 24"
+            style="enable-background:new 0 0 512 512"
+            xml:space="preserve"
+            >
+            <g>
+                <path
+                d="M9.707 19.121a.997.997 0 0 1-1.414 0l-5.646-5.647a1.5 1.5 0 0 1 0-2.121l.707-.707a1.5 1.5 0 0 1 2.121 0L9 14.171l9.525-9.525a1.5 1.5 0 0 1 2.121 0l.707.707a1.5 1.5 0 0 1 0 2.121z"
+                fill="currentColor"
+                data-original="#000000"
+                ></path>
+            </g>
+            </svg>
+        </span>
+    </button>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    /* From Uiverse.io by Juanes200122 */ 
+/* tooltip settings 👇 */
+
+.copy {
+  /* button */
+  --button-bg: #464646;
+  --button-hover-bg: #5a5a5a;
+  --button-text-color: #8bb9fe;
+  --button-hover-text-color: #8bb9fe;
+  --button-border-radius: 10px;
+  --button-diameter: 36px;
+  --button-outline-width: 1px;
+  --button-outline-color: rgb(141, 141, 141);
+  /* tooltip */
+  --tooltip-bg: #f4f3f3;
+  --toolptip-border-radius: 4px;
+  --tooltip-font-family: Menlo, Roboto Mono, monospace;
+  /* 👆 this field should not be empty */
+  --tooltip-font-size: 12px;
+  /* 👆 this field should not be empty */
+  --tootip-text-color: rgb(50, 50, 50);
+  --tooltip-padding-x: 7px;
+  --tooltip-padding-y: 7px;
+  --tooltip-offset: 8px;
+  --tooltip-transition-duration: 0.5s;
+  /* 👆 if you need a transition, 
+  just remove the comment,
+  but I didn't like the transition :| */
+}
+
+.copy {
+  box-sizing: border-box;
+  width: var(--button-diameter);
+  height: var(--button-diameter);
+  border-radius: var(--button-border-radius);
+  background-color: var(--button-bg);
+  color: var(--button-text-color);
+  border: none;
+  cursor: pointer;
+  position: relative;
+  outline: none;
+}
+
+.tooltip {
+  position: absolute;
+  opacity: 1;
+  visibility: visible;
+  top: 50%;
+  left: -190%;
+  transform: translateY(-50%);
+  white-space: nowrap;
+  font-size: 12px;
+  color: #fff;
+  background: #333;
+  padding: 6px 12px;
+  border-radius: 4px;
+  pointer-events: none;
+  transition: all 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+
+.tooltip::before {
+  content: attr(data-text-initial);
+}
+
+.tooltip::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -1px;
+  width: 12px;
+  height: 12px;
+  background: inherit;
+  transform: translateY(-50%) rotate(45deg);
+  z-index: 999;
+  pointer-events: none;
+}
+
+.copy svg {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.checkmark {
+  display: none;
+}
+
+/* actions */
+
+.copy:hover .tooltip,
+.copy:focus:not(:focus-visible) .tooltip {
+  opacity: 1;
+  visibility: visible;
+  top: calc((100% + var(--tooltip-offset)) * -1);
+}
+
+.copy:focus:not(:focus-visible) .tooltip::before {
+  content: attr(data-text-end);
+}
+
+.copy:focus:not(:focus-visible) .clipboard {
+  display: none;
+}
+
+.copy:focus:not(:focus-visible) .checkmark {
+  display: block;
+}
+
+.copy:hover,
+.copy:focus {
+  background-color: var(--button-hover-bg);
+}
+
+.copy:active {
+  outline: var(--button-outline-width) solid var(--button-outline-color);
+}
+
+.copy:hover svg {
+  color: var(--button-hover-text-color);
+}
+
+</style>
--- resources/js/Pages/Component/Show.vue
+++ resources/js/Pages/Component/Show.vue
@@ -6,7 +6,7 @@
                 <span></span>
                 <!-- <Back2 /> -->
                 <div class="flex items-center space-x-5">
-                    <span class="text-gray-100 font-bold">{{ component.name }}</span>
+                    <span class="text-gray-100 font-bold text-lg">{{ component.name }}</span>
                     <p class="text-gray-400 flex items-center space-x-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
@@ -19,7 +19,7 @@
 
             <section class="flex m-2">
                 <!-- parte izquierda (componente) -->
-                <article class="bg-gray-300 w-1/2 rounded-l-3xl h-[500px] flex items-center justify-center relative">
+                <article :class="['w-1/2 rounded-l-3xl h-[600px] flex items-center justify-center relative', darkMode ? 'bg-black' : 'bg-['+ customColor + ']']">
                     <button @click="goBack" class="absolute top-2 left-2 size-12 text-white rounded-full flex items-center justify-center bg-gray-400 opacity-70 hover:scale-105">
                         <i class="fa-solid fa-arrow-left"></i>
                     </button>
@@ -27,15 +27,80 @@
                     <!-- Vista previa del componente -->
                     <div v-html="component.html_code"></div>
 
-                    <!-- acciones -->
-                    <div class="absolute left-2 top-16 rounded-full h-72 w-12 p-4 bg-gray-400 opacity-50 flex flex-col">
-                        
+                    <div class="flex items-center space-x-3 absolute top-2 right-2 z-20 bg-gray-500 opacity-75 rounded-lg py-1 px-3">
+                        <!-- Toggle para cambiar a modo oscuro -->
+                        <!-- <div class="">
+                            <label for="darkModeToggle" class="text-white mr-2">Modo oscuro</label>
+                            <input type="checkbox" id="darkModeToggle" v-model="darkMode" class="cursor-pointer">
+                        </div> -->
+                        <span class="text-white font-bold">{{ customColor }}</span>
+                        <!-- Selector de color -->
+                        <div class="flex items-center">
+                            <label for="colorPicker" class="text-white mr-2">Color de fondo</label>
+                            <input type="color" id="colorPicker" v-model="customColor" class="cursor-pointer">
+                        </div>
                     </div>
                 </article>
 
-                <!-- parte derecha (código) -->
-                <article class="bg-slate-900 w-1/2 rounded-r-3xl h-[500px]">
+                <!-- Parte derecha (código) -->
+                <article class="bg-slate-900 w-1/2 rounded-r-3xl h-[600px] flex flex-col">
+                    <!-- Pestañas -->
+                    <div class="flex space-x-4 border-b border-gray-200 bg-gradient-to-t from-gray-600 to-gray-900">
+                    <button
+                        v-for="tab in tabs"
+                        :key="tab"
+                        @click="selectedTab = tab"
+                        :class="{
+                        'border-blue-500 text-blue-500': selectedTab === tab,
+                        'border-transparent text-gray-400 hover:text-gray-300': selectedTab !== tab
+                        }"
+                        class="py-2 px-4 border-b-2 font-medium text-sm focus:outline-none z-10"
+                    >
+                        {{ tab }}
+                    </button>
+                    </div>
 
+                    <!-- Contenedor del código con scroll y números de línea -->
+                    <div class="flex-1 overflow-auto relative">
+                        <!-- Botón de copiar -->
+                        <div v-if="(selectedTab === 'HTML' && component.html_code) || (selectedTab === 'CSS' && component.css_code) || (selectedTab === 'JS' && component.js_code)" class="flex justify-end absolute top-2 right-2">
+                            <CopyButton @click="copyToClipboard" />
+                        </div>
+
+                    <!-- Contenido de las pestañas -->
+                    <div v-if="selectedTab === 'HTML'" class="bg-gray-900 p-3 rounded-lg mt-7">
+                        <div class="flex">
+                        <!-- Números de línea -->
+                        <div class="text-gray-500 pr-4 text-right select-none">
+                            <pre v-for="(line, index) in getLines(component.html_code)" :key="index" class="m-0">{{ index + 1 }}</pre>
+                        </div>
+                        <!-- Código -->
+                        <pre class="text-white flex-1 overflow-x-auto"><code>{{ component.html_code ?? 'No hay código html' }}</code></pre>
+                        </div>
+                    </div>
+
+                    <div v-if="selectedTab === 'CSS'" class="bg-gray-900 p-3 rounded-lg">
+                        <div class="flex">
+                        <!-- Números de línea -->
+                        <div class="text-gray-500 pr-4 text-right select-none">
+                            <pre v-for="(line, index) in getLines(component.css_code)" :key="index" class="m-0">{{ index + 1 }}</pre>
+                        </div>
+                        <!-- Código -->
+                        <pre class="text-white flex-1 overflow-x-auto"><code>{{ component.css_code ?? 'No hay código CSS' }}</code></pre>
+                        </div>
+                    </div>
+
+                    <div v-if="selectedTab === 'JS'" class="bg-gray-900 p-3 rounded-lg">
+                        <div class="flex">
+                        <!-- Números de línea -->
+                        <div class="text-gray-500 pr-4 text-right select-none">
+                            <pre v-for="(line, index) in getLines(component.js_code)" :key="index" class="m-0">{{ index + 1 }}</pre>
+                        </div>
+                        <!-- Código -->
+                        <pre class="text-white flex-1 overflow-x-auto"><code>{{ component.js_code ?? 'No hay código js' }}</code></pre>
+                        </div>
+                    </div>
+                    </div>
                 </article>
             </section>
         </div>
@@ -45,14 +110,19 @@
 <script>
 import { Head } from '@inertiajs/vue3';
 import Back2 from "@/Components/MyComponents/Back2.vue";
+import CopyButton from "@/Components/MyComponents/CopyButton.vue";
 
 export default {
 data() {
     return {
-
+        tabs: ['HTML', 'CSS', 'JS'],
+        selectedTab: 'HTML',
+        darkMode: false, // Estado del modo oscuro
+        customColor: '#cfcfcf', // Color personalizado por el usuario
     }
 },
 components:{
+    CopyButton,
     Back2,
     Head
 },
@@ -60,6 +130,21 @@ props:{
     component: Object
 },
 methods:{
+     // Método para copiar el contenido al portapapeles
+    copyToClipboard() {
+      const code = this.selectedTab === 'HTML' ? this.component.html_code :
+                  this.selectedTab === 'CSS' ? this.component.css_code :
+                  this.component.js_code;
+
+      navigator.clipboard.writeText(code).then(() => {
+      }).catch(() => {
+        alert('Error al copiar el código');
+      });
+    },
+    // Método para obtener las líneas del código
+    getLines(code) {
+      return code ? code.split('\n') : [];
+    },
     formatViews(views) {
         if (views >= 1_000_000) {
             return (views / 1_000_000).toFixed(1) + 'M';
